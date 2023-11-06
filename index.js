4d0e247f32104cd3e0f9ff4ab68fa3d86b4a1380
Hannah Lee <106423868+hannahlee2021@users.noreply.github.com>
2023-11-06 18:09:15 -0500

    init

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello")
+// console.log("hello")
+
+//import the express library amd assign it to a variable
+import express from 'express'
+
+//create an instance of express application
+const app = express();
+
+//set the port the application will run on
+const port = process.env.PORT || 3001
+
+//set up response for the root path of application
+// / = root
+app.get('/', (req,res)=> {
+    res.send("draw an orange")
+})
+
+//set up the application to listen on the specified port 
+app.listen(port,() => {
+    console.log(`example app listening on port ${port}`)
+
+})
